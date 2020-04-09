2f44b3f078bfc61ee21414f88b351b82e60f38ca
AZZABI Radhouene 234587 <RA234587@intra.cea.fr>
2020-04-09 15:26:33 +0200

    delete flowstats

--- components/FlowhubStats.js
+++
@@ -1,8 +0,0 @@
-const noflo = require('noflo');
-
-function getFlowhubStats() {
-  return fetch('https://api.flowhub.io/stats')
-    .then(result => result.json());
-}
-
-exports.getComponent = () => noflo.asComponent(getFlowhubStats);
